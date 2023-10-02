e47d99bc76ed7fe6b42ce50bcc014d68c87c755e
adolpho-cavalcanti <acn2011@outlook.com>
2023-10-02 17:56:20 -0300

    feat: Configurando eslint e prettier

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  const abc = 'abc'
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h2>{abc}</h2>
+    </main>
+  )
+}
